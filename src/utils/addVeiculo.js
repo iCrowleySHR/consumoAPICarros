89f88a1a38e620850895a8ad4f7944b71f0abc3b
Gustavo Gualda <104951600+iCrowleySHR@users.noreply.github.com>
2024-01-18 19:05:31 -0300

    Update addVeiculo.js

--- src/utils/addVeiculo.js
+++ src/utils/addVeiculo.js
@@ -2,12 +2,9 @@ let linkAPI = "http://localhost/github/ApiCarros/api/v1/cars"
 export function submit(dados){
     fetch("http://localhost/github/ApiCarros/api/v1/cars", {
         method: 'GET',
+        mode: 'no-cors',
         headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': 'http://localhost'
-            'Access-Control-Allow-Methods': 'GET, POST, OPTIONS'
-            'Access-Control-Allow-Headers': 'Content-Type'
-            'Access-Control-Allow-Credentials': 'true'
+            'Content-Type': 'application/json'
           }
       })
         .then(resposta => resposta.json())
