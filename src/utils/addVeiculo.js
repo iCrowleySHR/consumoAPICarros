fe673d51bd9abb87b08990087d2f2438ee651a77
Gustavo Gualda <104951600+iCrowleySHR@users.noreply.github.com>
2024-01-21 18:44:50 -0300

    update

--- src/utils/addVeiculo.js
+++ src/utils/addVeiculo.js
@@ -30,6 +30,12 @@ export const fetchTransmissão = () => {
 /*======================================================================================================================= */
 /*ENVIAR DADOS PARA API */
 export function submit(dadosUsuario) {
+  let username = '123'
+  let password = '123'
+
+  const credentials = btoa(`${username}:${password}`);
+  console.log(credentials)
+
   let valoresEnviar = [
     {
       "valor": dadosUsuario.valor,
@@ -62,11 +68,35 @@ export function submit(dadosUsuario) {
 
   fetch('http://localhost/github/ApiCarros/api/v1/cars', {
       method: 'POST',
-      headers:{
-          "Content-Type": "application/json",
-          'Authorization': 'Basic YWRtaW5AZW1haWwuY29tOmFkbWlu'
-      },
-      body:JSON.stringify(valoresEnviar)
+      headers: {
+    'Content-Type': 'application/json',
+    'Authorization': `Basic ${credentials}`,
+  },
+      body:JSON.stringify({
+        "valor": "54890.55",
+        "id_modelo": 4,
+        "id_combustivel": 2,
+        "id_transmissao": 1,
+        "versao": "1.8 LT SPORT6 16V FLEX 4P MANUAL",
+        "imagem_um": "https://http2.mlstatic.com/D_NQ_NP_732550-MLB73912531540_012024-O.webp",
+        "imagem_dois": "https://http2.mlstatic.com/D_NQ_NP_724354-MLB73912531550_012024-O.webp",
+        "imagem_tres": null,
+        "ano_producao": 2013,
+        "ano_lancamento": 2013,
+        "portas": 4,
+        "motor": "1.8",
+        "carroceria": "Hatch",
+        "piloto_automatico": false,
+        "climatizador": true,
+        "vidro_automatico": true,
+        "am_fm": true,
+        "entrada_auxiliar": false,
+        "bluetooth": false,
+        "cd_player": false,
+        "dvd_player": false,
+        "leitor_mp3": false,
+        "entrada_usb": false
+      })
     })
       .then(resposta => resposta.json())
       .then(resultadoEnvio => {
